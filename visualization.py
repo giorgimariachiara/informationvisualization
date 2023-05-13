4879a84e6d3990be468ab24ae58884c76442bdf3
Maria Chiara <mariachiara.giorgi1@gmail.com>
2023-05-13 21:43:54 +0200

    f

--- visualization.py
+++ visualization.py
@@ -1,45 +1,65 @@
+# importing geopy library
 from geopy.geocoders import Nominatim
 import folium
 import pandas as pd
 from IPython.display import IFrame
 
-df = pd.read_csv('femalecities.csv')
-# calling the Nominatim tool
+# Read the CSV file
+df = pd.read_csv('deputies.csv')
 
+# Create a dictionary to map region to icon color
+icon_dict = {'Nord': 'blue', 'Centro': 'green', 'Sud': 'red'}
 
+# Calculate the city counts
+city_counts_dict = df['città'].value_counts().to_dict()
+
+# Calling the Nominatim tool
 loc = Nominatim(user_agent="GetLoc")
-lat=[]
-long=[]
-region=[]
-# entering the location name
+
+# Create empty lists to store latitude, longitude, and region
+lat = []
+lon = []
+regione = []
+
+# Iterate over the DataFrame to get location coordinates and region
 for index, row in df.iterrows():
-    getLoc = loc.geocode(row[0])
+    getLoc = loc.geocode(row['città'])
     if getLoc is not None:
         lat.append(getLoc.latitude)
-        long.append(getLoc.longitude)
-        region.append(row[1]) # aggiungi la regione di appartenenza della città
-df_cord = pd.concat([pd.Series(lat, name='Lat'), pd.Series(long, name='Long'), pd.Series(region, name='Region')], axis=1)
-m = folium.Map(df_cord[['Lat', 'Long']].mean().values.tolist())
+        lon.append(getLoc.longitude)
+        regione.append(row['regione'])
+    
+# Create a DataFrame to store the coordinates and region
+df_cord = pd.concat([pd.Series(lat, name='lat'), pd.Series(lon, name='lon'), pd.Series(regione, name='regione')], axis=1)
 
-# definisci un dizionario di icone, una per ogni regione
-icon_dict = {
-    'Nord': 'cloud',
-    'Centro': 'star',
-    'Sud': 'heart'
-}
+print(type(df))
+"""
+# Create a folium map
+m = folium.Map(df_cord[['lat', 'lon']].mean().values.tolist())
 
-for lat, lon, reg in zip(df_cord['Lat'], df_cord['Long'], df_cord['Region']):
-    # utilizza la funzione icon di Folium per specificare l'icona da utilizzare per ogni marker
-    folium.Marker(location=[lat, lon], icon=folium.Icon(icon=icon_dict[reg])).add_to(m)
+# Iterate over the DataFrame to add markers and set the marker color based on the count
+for index, row in df.iterrows():
+    city = row['città']
+    count = city_counts_dict[city]
+    color = 'red' if count > 10 else 'green' if count > 50 else 'blue'
+    folium.Marker([row['lat'], row['lon']], icon=folium.Icon(color=color), popup=f"{city}: {count}").add_to(m)
 
-sw = df_cord[['Lat', 'Long']].min().values.tolist()
-ne = df_cord[['Lat', 'Long']].max().values.tolist()
+# Iterate over the DataFrame to add markers and set the icon based on the region
+for lat, lon, regione in zip(df_cord['lat'], df_cord['lon'], df_cord['regione']):
+    # use the icon function of Folium to specify the icon to use for each marker
+    folium.Marker(location=[lat, lon], icon=folium.Icon(icon=icon_dict[regione])).add_to(m)
 
+# Fit the map bounds and save it to an HTML file
+sw = df_cord[['lat', 'lon']].min().values.tolist()
+ne = df_cord[['lat', 'lon']].max().values.tolist()
 m.fit_bounds([sw, ne])
-map2_html = m._repr_html_()
-m.save('map2.html')
-IFrame(src='map2.html', width='100%', height='500px')
+m.save('map3.html')
 
+# Display the map in a Jupyter notebook using an IFrame
+IFrame(src='map3.html', width='100%', height='500px')
+
+
+"""
 
 """
 endpoint = "https://dati.camera.it/sparql"
